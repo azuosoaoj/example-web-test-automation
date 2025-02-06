a7b6dfa0cdd7e58fdb9975e76ea395b0b4f37f8c
João Victor Garcia de Souza <71164239+azuosoaoj@users.noreply.github.com>
2025-02-06 15:24:17 -0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    baseURL: 'https://blog.agibank.com.br/'
+    baseURL: 'https://example.com/'
   },
 
   /* Configure projects for major browsers */
